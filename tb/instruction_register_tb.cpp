7d35e77655fbf2c06d0471072b01326d3f4ccb5c
Mariana Emerenciano <mariana.miranda.088@ufrn.edu.br>
2025-05-12 11:49:31 -0300

    instruction_register_tb 32bits

--- tb/instruction_register_tb.cpp
+++ tb/instruction_register_tb.cpp
@@ -1,15 +1,13 @@
-#include "systemc.h"
+#include <systemc.h>
 #include "instruction_register.h"
 
 int sc_main(int argc, char* argv[]) {
-    // Signals
-    sc_signal<sc_uint<64>> instructionIn;
-    sc_signal<sc_uint<64>> instructionOut;
+    sc_signal<sc_uint<32>> instructionIn;
+    sc_signal<sc_uint<32>> instructionOut;
     sc_signal<bool> enable;
     sc_signal<bool> write;
     sc_clock clock("clock", 10, SC_NS);
 
-    // Instantiate the module
     InstructionRegister ir("InstructionRegister");
     ir.instructionIn(instructionIn);
     ir.instructionOut(instructionOut);
@@ -17,7 +15,7 @@ int sc_main(int argc, char* argv[]) {
     ir.write(write);
     ir.clock(clock);
 
-    // Open VCD trace file
+    // VCD trace file
     sc_trace_file* tf = sc_create_vcd_trace_file("instruction_register_trace");
     sc_trace(tf, instructionIn, "instructionIn");
     sc_trace(tf, instructionOut, "instructionOut");
@@ -27,30 +25,24 @@ int sc_main(int argc, char* argv[]) {
 
     std::cout << "Running InstructionRegister testbench..." << std::endl;
 
-    // Simulation
-    enable.write(false);
-    write.write(false);
-    instructionIn.write(0xAAAAAAAAAAAAAAAA);
-    sc_start(10, SC_NS);  // 1st rising edge
-
+    // test 1: W on, E on
     enable.write(true);
     write.write(true);
-    instructionIn.write(0x1234567890ABCDEF);
-    sc_start(10, SC_NS);  // 2nd rising edge (write active)
-
+    instructionIn.write(0x12345678);
+    sc_start(10, SC_NS);
     std::cout << "[WRITE] instructionOut = 0x" << std::hex << instructionOut.read() << std::endl;
 
+    // Test 2: W off, E on
     write.write(false);
-    instructionIn.write(0xFFFFFFFFFFFFFFFF);  // Should be ignored
-    sc_start(10, SC_NS);  // 3rd rising edge (hold)
-
+    instructionIn.write(0x87654321); // shouldn't affect output
+    sc_start(10, SC_NS);
     std::cout << "[HOLD] instructionOut = 0x" << std::hex << instructionOut.read() << std::endl;
 
-    enable.write(false);  // Disabled
-    instructionIn.write(0x1111111111111111);
+    // Test 3: E off, W on
+    enable.write(false);
     write.write(true);
-    sc_start(10, SC_NS);  // 4th rising edge (disabled)
-
+    instructionIn.write(0xDEADBEEF); // should be ignored
+    sc_start(10, SC_NS);
     std::cout << "[DISABLED] instructionOut = 0x" << std::hex << instructionOut.read() << std::endl;
 
     std::cout << "InstructionRegister testbench completed." << std::endl;
